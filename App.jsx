6c1a766d38e82968cb1f2d75ca4d28e87a52d6d3
Amigauravsingh <gauravsinghmar786@gmail.com>
2024-02-25 20:54:40 +0530

    Your commit message here

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+
+const AppLayout = () => {
+    return (
+        <div className="heading">
+            <h1>hello world</h1>
+        </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Heading/>);
+
+
+
+
