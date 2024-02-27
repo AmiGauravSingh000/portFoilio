d99784834d4b9a4bde1d3373654b4cfb2dc88bbe
Amigauravsingh <gauravsinghmar786@gmail.com>
2024-02-27 07:03:04 +0530

    created a basic strucuture

--- App.jsx
+++ App.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-
+import Header from "./src/components/Header";
 
 
 const AppLayout = () => {
     return (
-        <div className="heading">
-            <h1>hello world</h1>
-        </div>
+      <>
+    <Header />
+      </>
     );
 };
 
-//git init
-//git remote
-// git add . 
-//git commit 
-//git push -
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppLayout/>);
 
